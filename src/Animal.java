aaedde1de763eb24d010c2991b1ee53e037c33bc
EcaMuntean <vultur_eca@yahoo.com>
2022-07-13 20:32:36 +0300

    Tema 13 Inheritance

---
+++ src/Animal.java
@@ -0,0 +1,101 @@
+public class Animal {
+    private int age ;
+    private String race ;
+    private String name ;
+    private String color ;
+    private double weight ;
+    private int heightCm ;
+    private int healthStatus ;
+    private int hungryStatus ;
+    private int spiritStatus ;
+    private String favouriteFood  ;
+    private String favouriteActivity;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getHeightCm() {
+        return heightCm;
+    }
+
+    public void setHeightCm(int heightCm) {
+        this.heightCm = heightCm;
+    }
+
+    public int getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(int healthStatus) {
+        this.healthStatus = healthStatus;
+    }
+
+    public int getHungryStatus() {
+        return hungryStatus;
+    }
+
+    public void setHungryStatus(int hungryStatus) {
+        this.hungryStatus = hungryStatus;
+    }
+
+    public int getSpiritStatus() {
+        return spiritStatus;
+    }
+
+    public void setSpiritStatus(int spiritStatus) {
+        this.spiritStatus = spiritStatus;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String getFavouriteActivity() {
+        return favouriteActivity;
+    }
+
+    public void setFavouriteActivity(String favouriteActivity) {
+        this.favouriteActivity = favouriteActivity;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
+
+
     public static Dog dog = new Dog();
+    public static Cat cat = new Cat();
+    public static Duck duck = new Duck();
     public static Adoption adoption = new Adoption();
     public static Food food = new Food();
     public static Activities activities = new Activities();
@@ -8,10 +12,30 @@ public class Main {
 
     public static void main(String[] args) {
         //Dog values
+        dog.setAge(5);
+        dog.setName("Ralph");
+        dog.setFavouriteActivity("sleeping");
+        dog.setFavouriteFood("NutriBest");
         System.out.println(dog.getName());
         System.out.println(dog.getAge());
         System.out.println(dog.getFavouriteActivity());
         System.out.println(dog.getFavouriteFood());
+
+        //Cat values
+        cat.setAge(2);
+        cat.setColor("black");
+        cat.setName("Timi");
+        cat.setHealthStatus(4);
+        System.out.println(cat.getAge());
+        System.out.println(cat.getColor());
+        System.out.println(cat.getName());
+        System.out.println(cat.getHealthStatus());
+
+        //Duck values
+        duck.setRace("decorative");
+        duck.setFavouriteFood("corn");
+        System.out.println(duck.getRace());
+        System.out.println(duck.getFavouriteFood());
 
         //Adoption values
         System.out.println(adoption.getName());
